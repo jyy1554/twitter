f7bc0879f62b0599f6cfe18f88519781b7c486c4
jyy1554 <jyy1556@gmail.com>
2022-03-31 20:07:13 +0900

    자신의 트윗만 보이게 하기

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,30 @@
 import { signOut } from "firebase/auth";
-import React from "react";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { authService } from "../fbase";
+import { authService, dbService } from "../fbase";
 
-function Profile() {
+function Profile({ userObj }) {
     const navigate = useNavigate();
 
     const onLogOutClick = () => {
         signOut(authService);
         navigate("/");
     };
+    const getMyTweets = async () => {
+        const q = query(
+            collection(dbService, "tweets"),
+            where("creatorId", "==", userObj.uid),
+            orderBy("createdAt", "desc")
+        );
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            console.log(doc.data());
+        });
+    };
+    useEffect(() => {
+        getMyTweets();
+    }, []);
 
     return (
         <div>
