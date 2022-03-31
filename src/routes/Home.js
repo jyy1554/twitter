36d78ab03f452eb070541c8ddef6cac00ffebd3e
jyy1554 <jyy1556@gmail.com>
2022-03-31 18:18:31 +0900

    image useState 만들기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -13,6 +13,7 @@ import Tweet from "../components/Tweet";
 function Home({ userObj }) {
     const [tweet, setTweet] = useState("");
     const [tweets, setTweets] = useState([]);
+    const [attachment, setAttachment] = useState();
 
     useEffect(() => {
         const q = query(
@@ -42,6 +43,20 @@ function Home({ userObj }) {
         const { value } = e.target;
         setTweet(value);
     };
+    const onFileChange = (e) => {
+        const { files } = e.target;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            //리스너 만들기
+            const {
+                currentTarget: { result },
+            } = finishedEvent;
+            setAttachment(result);
+        };
+        reader.readAsDataURL(theFile);
+    };
+    const onClearAttachment = () => setAttachment(null);
 
     return (
         <div>
@@ -53,7 +68,14 @@ function Home({ userObj }) {
                     placeholder="What's on your mind?"
                     maxLength={120}
                 />
+                <input type="file" accept="image/*" onChange={onFileChange} />
                 <input type="submit" value="Tweet" />
+                {attachment && (
+                    <div>
+                        <img src={attachment} width="50px" height="50px" />
+                        <button onClick={onClearAttachment}>Clear</button>
+                    </div>
+                )}
             </form>
             <div>
                 {tweets.map((tweet) => (
